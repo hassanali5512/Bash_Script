c12ae18e6a629da527a59e1b04755da9f4495934
hassanali5512 <imhassan5512@gmail.com>
2024-09-05 16:48:16 -0700

    Update install.jenkins.sh

--- install.jenkins.sh
+++ install.jenkins.sh
@@ -8,3 +8,4 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+eecho "Installation Finished"
