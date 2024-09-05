09bc9232e5450e9a21665aabf02b84bb2b6ca404
hassanali5512 <imhassan5512@gmail.com>
2024-09-05 15:35:16 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash/
 echo "Welcome to my deployment"
-if [ ! -d "/home/ubuntu/demo1/Hassan" ]; then
+if dpkg -l | grep -qw nginx; then
+     echo "Nginx is already installed"
+else
+     echo "Nginx is not installed.Installing nginx..."
+     sudo apt install nginx -y
+     systemctl start nginx
+fi
+systemctl status nginx --no-pager
+
+if [ ! -d "/home/ubuntu/demo1/Hassan/" ]; then
         cd /home/ubuntu/demo1/
         git clone https://github.com/hassanali5512/Hassan.git
-        cd Hassan
+        cd Hassan/
 
 else
         cd /home/ubuntu/demo1/Hassan
-        git pull
+        git pull origin main
 fi
   sudo cp * /var/www/html/
   echo "End"
